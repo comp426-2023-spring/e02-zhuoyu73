ab9e7ecaa5e0a3b66bd6e3370e5020c2fc623d5b
Zhuoyu Shi <zhuoyu73@email.unc.edu>
2023-05-09 02:05:37 -0400

    Edit

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 //// Load most basic dependencies
 // Create require function 
 // https://nodejs.org/docs/latest-v18.x/api/module.html#modulecreaterequirefilename
@@ -102,3 +103,50 @@ process.on('SIGINT', () => {
         }    
     })
 })
+
+
+import { rps, rpsls } from "./lib/rpsls.js"
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
+app.get('/app', (req, res) => {
+    res.status(200).send('200 OK');
+})
+
+app.get('/app/rps', (req, res) => {
+    res.status(200).send(rps());
+})
+
+app.get('/app/rpsls', (req, res) => {
+    res.status(200).send(rpsls());
+})
+
+app.get('/app/rps/play', (req, res) => {
+    res.status(200).send(rps(req.query.shot));
+})
+
+app.get('/app/rpsls/play', (req, res) => {
+    res.status(200).send(rpsls(req.query.shot));
+})
+
+app.post('/app/rps/play', (req, res) => {
+    res.status(200).send(rps(req.body.shot));
+})
+
+app.post('/app/rpsls/play', (req, res) => {
+    res.status(200).send(rpsls(req.body.shot));
+})
+
+app.get('/app/rps/play/:shot', (req, res) => {
+    res.status(200).send(rps(req.params.shot));
+})
+
+app.get('/app/rpsls/play/:shot', (req, res) => {
+    res.status(200).send(rpsls(req.params.shot));
+})
+
+app.get('*', (req, res) => {
+    res.status(404).send('404 NOT FOUND');
+  });
